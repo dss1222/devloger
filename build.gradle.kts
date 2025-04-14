19e026bc0c65ea1ae1f0d8d3eac67f835c10fb3c
신동석 <96603945+dss1222@users.noreply.github.com>
2025-04-14 21:02:48 +0900

    auth-service Eureka 클라이언트 등록 및 spring cloud 호환 설정 완료

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,16 @@ plugins {
     id("io.spring.dependency-management") version "1.1.3"
 }
 
-allprojects {
+subprojects {
+    apply(plugin = "io.spring.dependency-management")
+
+    dependencyManagement {
+        imports {
+            mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.1")
+        }
+    }
+
     repositories {
         mavenCentral()
-    }
-}
-
-dependencyManagement {
-    imports {
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2022.0.4")
     }
 }
